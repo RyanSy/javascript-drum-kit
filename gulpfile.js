6f66c6a1e7477025fcf12646793a10c1df72879f
Ryan Sy <swerve@MacBook-Pro.local>
2017-10-18 14:18:58 -0400

    refactored gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,18 @@ var pump = require('pump');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 
-gulp.task('html', function() {
-  return gulp.src('public/index.html')
-    .pipe(gulp.dest('dist/public'));
+gulp.task('move files', function() {
+  return gulp.src(['public/index.html', 'public/img/*.*', 'public/sounds/*.*', 'server.js', 'package.json'], {base: './'})
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('css', function() {
+gulp.task('clean css', function() {
   return gulp.src('public/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/public/css'));
 });
 
-gulp.task('js', function(cb) {
+gulp.task('uglify js', function(cb) {
   pump([
       gulp.src('public/js/*.js'),
       babel({
@@ -28,24 +28,4 @@ gulp.task('js', function(cb) {
   );
 });
 
-gulp.task('img', function() {
-  return gulp.src('public/img/*.*')
-    .pipe(gulp.dest('dist/public/img'));
-});
-
-gulp.task('sounds', function() {
-  return gulp.src('public/sounds/*.*')
-    .pipe(gulp.dest('dist/public/sounds'));
-});
-
-gulp.task('server', function() {
-  return gulp.src('server.js')
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('package.json', function() {
-  return gulp.src('package.json')
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', ['html', 'css', 'js', 'img', 'sounds', 'server', 'package.json']);
+gulp.task('default', ['move files', 'clean css', 'uglify js']);
